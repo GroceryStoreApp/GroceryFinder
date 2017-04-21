6e21974007e7485d2bd810aedc2c1ec9b8e1f62f
kaimihata <kaimihata@gmail.com>
2017-04-21 13:32:30 -0700

    Added newInstance

---
+++ app/src/main/java/com/groceryfinder/android/groceryfinder/GroceryListActivity.java
@@ -0,0 +1,14 @@
+package com.groceryfinder.android.groceryfinder;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by KaiMacBookAir on 4/21/17.
+ */
+
+public class GroceryListActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment createFragment() {
+        return new GroceryListFragment.succ();
+    }
+}
---
+++ app/src/main/java/com/groceryfinder/android/groceryfinder/GroceryListFragment.java
@@ -0,0 +1,14 @@
+package com.groceryfinder.android.groceryfinder;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by KaiMacBookAir on 4/21/17.
+ */
+
+public class GroceryListFragment extends Fragment {
+
+    public static GroceryListFragment succ() {
+        return new GroceryListFragment();
+    }
+}
